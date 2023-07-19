7698d1e4fedaa82cbf81a1108efbb68fe05bb3ee
Vladimir Ryabenko <v.ryabenko@innopolis.university>
2023-07-19 11:38:28 +0300

    tracing fixed

--- cmd/tracing/tracing.go
+++ cmd/tracing/tracing.go
@@ -1,20 +1,28 @@
 package tracing
 
 import (
+	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go/config"
 	"go.uber.org/zap"
 )
 
 func InitTracing(serviceName string, logger *zap.Logger) {
 	cfg := config.Configuration{
+		ServiceName: serviceName,
 		Sampler: &config.SamplerConfig{
 			Type:  "const",
 			Param: 1,
 		},
+		Reporter: &config.ReporterConfig{
+			LogSpans:           true,
+			LocalAgentHostPort: "jaeger:6831",
+		},
 	}
 
-	_, err := cfg.InitGlobalTracer(serviceName)
+	tracer, _, err := cfg.NewTracer()
 	if err != nil {
 		logger.Fatal("Cannot init tracing", zap.Error(err))
 	}
+
+	opentracing.SetGlobalTracer(tracer)
 }
